4efdc88746f5ac3e947042070790fd8478b79473
Ben <xuben1996@gmail.com>
2022-06-30 00:21:03 -0400

    Added routes and models

--- Pokemon_Ecommerce_API/routes/auth.js
+++ Pokemon_Ecommerce_API/routes/auth.js
@@ -1,3 +1,58 @@
 const router = require('express').Router();
+const User = require('../Models/User');
+const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+//REGISTER
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json('Wrong Credentials');
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const newPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if (newPassword !== req.body.password) {
+      return res.status(401).json('Wrong Credentials');
+    } else {
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        { expiresIn: '3d' }
+      );
+      const { password, ...others } = user._doc;
+      return res.status(200).json({ ...others, accessToken });
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
