36e8cedc8682002cb50f904e4f9cbdf406a97665
Ben <xuben1996@gmail.com>
2022-07-02 01:19:09 -0400

    Restructured folder

--- server/Pokemon_Ecommerce_API/routes/stripe.js
+++ server/Pokemon_Ecommerce_API/routes/stripe.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 router.post("/payments",(req,res) => {
-    stripe.charges.create
+    stripe.charges.c
 }
 
 module.exports = router;
